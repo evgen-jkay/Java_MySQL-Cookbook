58f676667bc3be389bcdd3bc8340bd3a24a0beda
evgen-jkay <evgenlandarenko@gmail.com>
2023-08-19 20:37:50 +0300

    add all

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Main {
+    public static void main(String[] args) {
+        Connection conn = null;
+        String url = "jdbc:mysql://localhost/cookbook";
+        String userName = "root";
+        String password = "Qwerty335906";
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            conn = DriverManager.getConnection(url, userName, password);
+            System.out.println("Connected");
+        } catch (Exception e) {
+            System.out.println("Cannot connect to server");
+        } finally {
+            if (conn != null) {
+                try {
+                    conn.close();
+                    System.out.println("Disconnected");
+                } catch (Exception e) { }
+            }
+        }
+    }
+}
